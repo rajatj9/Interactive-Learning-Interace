5af803839d3ba8053bfca8602f576bf1c4236dc8
Rajat Jain <jain.rajat50@gmail.com>
2019-09-21 15:35:31 +0800

    Fixed Display big

--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -1,17 +1,43 @@
+
 <template>
   <div class="thanks-page">
-    <message-page :title="'Thank you !'"
-                  :subTitle="'Answer successfully submitted'"></message-page>
+    <result
+          :subTitle="'Thank you for taking the survey!'" :score="result" 
+          :maxScore="maxScore" :correctAnswers="correctAnswers" 
+          :personalGoal="personalGoal" :pastPerformances="pastPerformances">
+    </result>
+   
   </div>
+  
 </template>
 
 <script>
-  import Message from '../components/message/Message.vue';
+  import ResultsPage from '../components/ResultsPage.vue';
 
   export default {
     name: "ThankYou",
     components: {
-      messagePage: Message
+      result: ResultsPage,
+
+    },
+    data: function() {
+      return {
+        result: 82,
+        maxScore: 100,
+        feedbackType: 1,
+        correctAnswers: [
+              { 'qno': 1, 'correctAnswer': "A", "correct": true, "mcq":true },
+              { 'qno': 2, 'correctAnswer': "C",  "correct": false, "mcq": true},              
+              { 'qno': 3, 'correctAnswer': "D", "correct": false, "mcq": true},
+              { 'qno': 4, 'correctAnswer': "\\sqrt{2}^{2\\log_{2}{3}} = 3","correct": false, "mcq": false, 'formulaAnswer': true}
+            ],
+        pastPerformances: {
+          'Quiz 1': 'B+',
+          'Quiz 2': 'A-',
+          'Quiz 3': 'A-'
+        },
+      personalGoal: 'A'
+      }
     }
   }
 </script>
